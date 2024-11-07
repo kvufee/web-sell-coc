a7dfc86b82ac1918ea4c92a7df39a7b170ac8a6b
Алексей Попов <pzns1337@gmail.com>
2024-11-07 16:01:00 +0300

    [MODIFY] add toggleDarken() and time function

--- script/main.js
+++ script/main.js
@@ -1,16 +1,31 @@
-const darkenButton = document.getElementById('darkenButton');
+(function () {
+    var startTime = new Date().getTime();
 
-let darkened = false;
+    window.addEventListener("load", function () {
+        var endTime = new Date().getTime();
+        var duration = endTime - startTime;
 
-function toggleDarken() {
-    if (!darkened) {
-        document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-        darkened = true;
-    } else {
-        document.body.style.backgroundColor = '';
-        document.body.style.color = '';
-        darkened = false;
+        var footer = document.querySelector("footer");
+        var loadInfo = document.createElement("p");
+        loadInfo.textContent = "Время загрузки страницы: " + duration + " мс";
+
+        footer.appendChild(loadInfo);
+    });
+    
+    const darkenButton = document.getElementById('darkenButton');
+
+    let darkened = false;
+
+    function toggleDarken() {
+        if (!darkened) {
+            document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+            darkened = true;
+        } else {
+            document.body.style.backgroundColor = '';
+            document.body.style.color = '';
+            darkened = false;
+        }
     }
-}
 
-darkenButton.addEventListener('click', toggleDarken);
+    darkenButton.addEventListener('click', toggleDarken);
+})();
